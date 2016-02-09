b7ba7a9af883afecfb8c3a1e99165bdddafcd5d1
David Roberts <dfroberts2@gmail.com>
2016-02-09 17:22:29 -0500

    Add methods to application to ensure user is logged in before navigating through app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :ensure_logged_in
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def ensure_logged_in
+    redirect_to login_path unless current_user
+  end
 end
