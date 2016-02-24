2fd4b2a5fffcb365255d0063e9407fd5c12f7f28
David Roberts <dfroberts2@gmail.com>
2016-02-24 15:35:38 -0500

    Modify $ format of dept report

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	$('.neg-pos-val').each(function(){
 		if (parseFloat($(this).html()) < 0 ) {
+			var val = $(this).html()
 			debugger
+			$(this).addClass("neg-val");
 		}
-	})
+	});
 });
