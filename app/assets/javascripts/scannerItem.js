495fcdd92e4e37b4aa582d793cd2156e09131f56
David Roberts <dfroberts2@gmail.com>
2016-03-06 15:32:37 -0500

    Mobile views for upc items

--- app/assets/javascripts/scannerItem.js
+++ app/assets/javascripts/scannerItem.js
@@ -14,11 +14,11 @@ $(document).ready(function(){
 			if (response.notice != undefined){
 				$("#flash-container").html(response.notice);
 			} else {
-			var append = "<div class='row form-group'><div class='col-md-3 col-xs-3'><input class='form-control' type='text' value='" + response.category + "' name='category_scanner[category_items_attributes][0][category_name]' id='category_scanner_category_items_attributes_0_category_name' /></div><div class='col-md-3 col-xs-3'><input class='form-control' type='text' value='" + response.quantity + "' name='category_scanner[category_items_attributes][0][quantity]' id='category_scanner_category_items_attributes_0_quantity' /></div><div class='col-md-3 col-xs-3'><input class='form-control' type='text' value='" + response.retail + "' name='category_scanner[category_items_attributes][0][retail_price]' id='category_scanner_category_items_attributes_0_retail_price' /></div><div class='col-md-3 col-xs-3'><input name='category_scanner[category_items_attributes][0][_destroy]' type='hidden' value='0' /><input type='checkbox' value='1' name='category_scanner[category_items_attributes][0][_destroy]' id='category_scanner_category_items_attributes_0__destroy' /><label for='category_scanner__destroy'>Delete Item?</label></div></div><input type='hidden' value='" + response.id + "' name='category_scanner[category_items_attributes][0][id]' id='category_scanner_category_items_attributes_0_id' />";
-			$(append).insertAfter(".edit_category_scanner input[name=authenticity_token]");
-			$(".clearable").each(function(){
-				$(this).val("")
-			});
+				var append = "<div class='row form-group'><div class='col-md-3 col-xs-3'><input class='form-control' type='text' value='" + response.category + "' name='category_scanner[category_items_attributes][0][category_name]' id='category_scanner_category_items_attributes_0_category_name' /></div><div class='col-md-3 col-xs-3'><input class='form-control' type='text' value='" + response.quantity + "' name='category_scanner[category_items_attributes][0][quantity]' id='category_scanner_category_items_attributes_0_quantity' /></div><div class='col-md-3 col-xs-3'><input class='form-control' type='text' value='" + response.retail + "' name='category_scanner[category_items_attributes][0][retail_price]' id='category_scanner_category_items_attributes_0_retail_price' /></div><div class='col-md-3 col-xs-3'><input name='category_scanner[category_items_attributes][0][_destroy]' type='hidden' value='0' /><input type='checkbox' value='1' name='category_scanner[category_items_attributes][0][_destroy]' id='category_scanner_category_items_attributes_0__destroy' /><label for='category_scanner__destroy'>Delete Item?</label></div></div><input type='hidden' value='" + response.id + "' name='category_scanner[category_items_attributes][0][id]' id='category_scanner_category_items_attributes_0_id' />";
+				$(append).insertAfter(".edit_category_scanner input[name=authenticity_token]");
+				$(".clearable").each(function(){
+					$(this).val("")
+				});
 			}
 		}).fail(function(response){
 			console.log("fail: " + response)
