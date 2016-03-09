80f3b498c6fb50922206849344847b5191d6fef0
David Roberts <dfroberts2@gmail.com>
2016-03-08 20:19:24 -0500

    Edit view formatting

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -5,8 +5,14 @@ $(document).ready(function(){
 
 		function drawBarChart() {
 			var dataArray = [["Category", "Retail Value", { role: "style" } ]]
-			$('.category-row').each(function() {
-				dataArray.push([$(this).find('.category-name').html(),parseFloat($(this).find('.retail-sum').html().replace(",","")),randomHexColor()])
+			var barColors = ["#AA9E39", "#4A2D74", "#277650", "#AA5639", "#807515", "#694F90", "#0F5935", "#D4856A", "#554C00", "#8E79AD", "#003B1F", "#FFC0AA", "#D4CA6A", "#2F1557", "#4A9470", "#803015", "#FFF6AA", "#1A053A", "#76B295", "#551600"]
+			$('.category-row').each(function(index) {
+				if (index < barColors.length) {
+					var barColor = barColors[index];
+				} else {
+					var barColor = randomHexColor();
+				}
+				dataArray.push([$(this).find('.category-name').html(),parseFloat($(this).find('.retail-sum').html().replace(",","")),barColor]);
 			})
 			var data = new google.visualization.arrayToDataTable(dataArray);
 			var view = new google.visualization.DataView(data)
@@ -15,6 +21,7 @@ $(document).ready(function(){
 	        	bar: {groupWidth: "95%"},
 	        	legend: { position: "none" },
 	        	height: 320,
+	        	width: 1000,
 	        	vAxis: {format: '$' },
 	        	hAxis: {textStyle: {fontSize: 12}}
 	      	};
